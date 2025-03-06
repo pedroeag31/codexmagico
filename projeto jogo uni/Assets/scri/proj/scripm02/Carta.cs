7e4e5dc98a2975d9ac5122a23ee863206e379e20
andinhomorais <142852486+andinhomorais@users.noreply.github.com>
2025-03-06 16:09:46 -0300

    0034

--- projeto jogo uni/Assets/scri/proj/scripm02/Carta.cs
+++ projeto jogo uni/Assets/scri/proj/scripm02/Carta.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
 
-public class Carta
+public class Carta : MonoBehaviour
 {
-    
+    public Sprite ImagemOculta; // Imagem oculta no início
+    public Sprite ImagemRevelada; // Imagem revelada quando o jogador interage
+    public int index; // Índice da carta
+
+    private JogoDaMemoria jogo;
+
+    void Start()
+    {
+        jogo = FindObjectOfType<JogoDaMemoria>();
+        GetComponent<SpriteRenderer>().sprite = ImagemOculta;
+    }
+
+    // Definir a imagem que será revelada
+    public void DefinirImagem(Sprite imagem)
+    {
+        ImagemRevelada = imagem;
+    }
+
+    // Função para ser chamada quando a carta for clicada
+    void OnMouseDown()
+    {
+        jogo.RevelarCarta(gameObject);
+    }
+
+    // Função para desabilitar a carta quando um par correto for feito
+    public void Desabilitar()
+    {
+        gameObject.SetActive(false);
+    }
 }
